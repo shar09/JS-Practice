03917685fa10ec8dda2f4f70270adc1486af9d06
shar09 <sharathtelu9@gmail.com>
2020-04-21 11:49:10 -0400

    Working on js objects and lists

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+// * PLURALSIGHT
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+
 // * REST PARAMETERS
 
 // function sendCars(day, ...carIds) {
@@ -148,6 +154,16 @@
 
 
 // ---------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+// * ELOQUENT JAVASCRIPT
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+
+
+
 // *FizzBuzz
 
 // let num = 1;
@@ -303,17 +319,208 @@
 // console.log(charCount("Kalakand", 'k'));
 
 // ---------------------------------------------------------------
+// * Sum of a Range
+
+// function range(start, end, step) {
+//     let arr = [];
+//     let steps = step || 1;
+//     if(start < end) {
+//         for(let i=start; i<=end; i+=steps) {
+//             arr.push(i);
+//         }
+//     }
+//     else {
+//         for(let i=start; i>=end; i-=steps) {
+//             arr.push(i);
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(range(15, 10, 2));
+
+// function sum(range) {
+//     let sum = 0;
+//     for(let value of range) {
+//         sum += value;
+//     }
+//     return sum;
+// }
+
+// console.log(sum(range(1,10)));
+
+// ---------------------------------------------------------------
+// * Reverese an array
+
+// function reverse(array) {
+//    let newArr = [];
+//    for(let i=array.length-1; i>=0; i--) {
+//       newArr.push(array[i]);
+//    }
+//    return newArr;
+// }
+
+// function reverseInPlace(array) {
+//     for(let i=0;i<Math.floor(array.length/2);i++) {
+//        let old = array[i];
+//        array[i] = array[array.length - 1 - i];
+//        array[array.length - 1 - i] = old;
+//     }
+//     return array;
+// }
+
+// let arr = [1,2,3,4];
+// console.log(reverse(arr));
+// console.log(reverseInPlace(arr));
+
+// ---------------------------------------------------------------
+// * Lists
+
+//ArraytoList
+//ListtoArray
+//prepend(n, list)
+//nth(list, n)
+
+// function arrayToList(array) {
+//    let list = null;
+//     for(let i=array.length-1;i>=0;i--) {
+//        list = {value: array[i], rest: list};
+//     }
+//     return list;
+// }
+
+// const array = [1, 2, 3, '4'];
+// console.log(arrayToList(array));
+
+// function listToArray(list) {
+//     let array = [];
+//     for(let node = list; node; node=node.rest) {
+//         array.push(node.value);
+//     }
+//     return array;
+// }
+
+// console.log(listToArray(arrayToList(array)));
+
+// function prepend(value, list) {
+//     return {value, rest: list}
+// }
+
+// console.log(prepend(0, arrayToList(array)));
+
+// function nth(list, n) {
+//     while(n>=0) {
+//         if(n===0) 
+//             return list.value;
+//         else
+//             return nth(list.rest, n-1);
+//     }
+// }
+
+// function nthTwo(list, n) {
+//    for(let node = list;node;node = node.rest) {
+//        if(n==0) {
+//            return node.value;
+//        }
+//        else {
+//            n = n-1;
+//        }
+//    }
+// }
+
+// console.log(nth(arrayToList(array), 3));
+// console.log(nthTwo(arrayToList(array), 3));
+
+// ---------------------------------------------------------------
+// * Object Deep Comparision
+
+function deepEqual(obj1, obj2) {
+//    for(key in obj1) {
+//        //console.log(key, obj1[key]);
+//        if(!(key in obj2)) {
+//            return false;
+//        }
+//        if(obj1[key]!==obj2[key]) {
+//            return false;
+//        }
+//        return true;
+//     }
+   for(key in obj2) {
+       console.log(key, obj2[key]);
+   }
+}
+
+let obj1 = {here: {is: "an"}, object: 2}
+let obj2 = {value: 1, set: 2}
+console.log(typeof obj1[object]);
+
+console.log(deepEqual(obj1, {here: {is: "an"}, object: 2}));
+
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+// * DATASTRUCTURES AND ALGORITHMS COURSE
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+
+// * Anagram Challenge
+
+// function anagram(string1, string2) {
+//     if(string1.length !== string2.length) {
+//         return false;
+//     }
+//         let obj1 = {};
+//         let obj2 = {};    
+//         for(let val of string1) {
+//             obj1[val] = (obj1[val] || 0) + 1;   
+//         }
+//         for(let val of string2) {
+//             obj2[val] = (obj2[val] || 0) + 1;
+//         }
+//         console.log(obj1);
+//         console.log(obj2); 
+//         for(key in obj1) {
+//             if(!(key in obj2)) {
+//                 return false;
+//             }
+//             if(obj1[key] !== obj2[key]) {
+//                 return false;
+//             }
+//         }
+//      return true;
+// }
+
+// console.log(anagram('sharr', 'dfhar'));
+
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+// * MISC
+// -------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------
+
+
+// ---------------------------------------------------------------
 // *Object test
 
 // var obj = {
 //     test: "test",
 //     hey: "namaste",
-//     ['zapak'] : 2
+//     ['zapak'] : 2,
+//     num: 0
 // };
 
 // obj['hello'] = 1;
 // console.log(Object.keys(obj));
 // console.log(obj['hey']);
+// console.log(typeof(obj['num']));
+
+// console.log(obj);
+// console.log("test" +obj);
+// console.log("test", obj);
+
+// let {name, age} = {name: 'shar', age: 18};
+// console.log(name, age);
+// let string = JSON.stringify(obj);
+// console.log(string["test"]);
 
 // ---------------------------------------------------------------
 
@@ -375,36 +582,32 @@
 // const sw = new Stopwatch();
 
 // ---------------------------------------------------------------
+// * Nested functions test
 
-// * Anagram Challenge
+// class App {
+//     constructor() {
+//         this.greet = "hello";     
+//     }
 
-function anagram(string1, string2) {
-    if(string1.length !== string2.length) {
-        return false;
-    }
-        let obj1 = {};
-        let obj2 = {};    
-        for(let val of string1) {
-            obj1[val] = (obj1[val] || 0) + 1;   
-        }
-        for(let val of string2) {
-            obj2[val] = (obj2[val] || 0) + 1;
-        }
-        console.log(obj1);
-        console.log(obj2); 
-        for(key in obj1) {
-            if(!(key in obj2)) {
-                return false;
-            }
-            if(obj1[key] !== obj2[key]) {
-                return false;
-            }
-        }
-     return true;
-}
+//     test() {
+//         let nested = function() {
+//             console.log("hello");
+//         }
+//         nested();
+//     }
+// }
 
-console.log(anagram('sharr', 'dfhar'));
+// function multi(factor) {
+//    function test(base) {
+//        return factor * base;
+//    }
+//    return test;
+// }
 
+// const show = multi(5);
+// console.log(show(2));
+
+// ---------------------------------------------------------------
 
 
 
