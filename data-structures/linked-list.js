046d8b3071c47a92d746b14bf3707c714f4714cd
shar09 <sharathtelu9@gmail.com>
2020-06-26 21:09:32 -0400

    Linked list push and pop

---
+++ data-structures/linked-list.js
@@ -0,0 +1,54 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        let node = new Node(val);
+        if(!this.head) {
+            this.head = node;
+            this.tail = this.head;
+        }
+        else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
+    }
+    pop() {
+        if(!this.head) {
+            return undefined;
+        }
+        let current = this.head;
+        let newTail = this.head;
+        while(current.next) {
+            newTail = current; 
+            current = current.next;   
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
+
+let list = new LinkedList();
+console.log(list.push(6));
+console.log(list.push(7));
+console.log(list.pop());
+console.log(list.pop());
+console.log(list);
+
