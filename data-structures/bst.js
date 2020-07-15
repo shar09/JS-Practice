c7ad89e0e541de22cb6c9466214ff4321a197cb1
shar09 <sharathtelu9@gmail.com>
2020-07-15 11:40:42 -0400

    bst insert added

--- data-structures/bst.js
+++ data-structures/bst.js
@@ -10,4 +10,44 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
-}
+    insert(value) { 
+       let node  = new Node(value);
+       let iterate = true;
+       let current = this.root
+       if(!this.root) {
+        this.root = node;
+        return this;
+       }
+       while(iterate) {
+            while(node.value < current.value) {
+                if(!current.left) {
+                    current.left = node;
+                    iterate = false;
+                    return this;
+                }
+                else {
+                    current = current.left;
+                }
+            }
+            while(node.value > current.value) {
+                if(!current.right) {
+                    current.right = node;
+                    iterate = false;
+                    return this;
+                }
+                else {
+                    current = current.right;
+                }
+            }
+        }
+    }
+}    
+
+let bst = new BinarySearchTree();
+bst.insert(3);
+bst.insert(2);
+bst.insert(4);
+bst.insert(1);
+bst.insert(10);
+bst.insert(6);
+console.log(bst);
