f0f132ff815b81c341299c06259497d31da50ae3
shar09 <sharathtelu9@gmail.com>
2020-07-16 12:08:23 -0400

    dfs in order added

---
+++ algorithms/tree-traversal/dfs-in.js
@@ -0,0 +1,29 @@
+let { BinarySearchTree } = require('../../data-structures/bst')
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+//console.log(tree);
+
+function dfsIn(tree) {
+    current = tree.root;
+    let visited = [];
+    function traverse(current) {
+        if(current.left) {
+           traverse(current.left);
+        }
+        visited.push(current.value);
+        if(current.right) {
+            traverse(current.right);
+        }
+        return;
+    }
+    traverse(current);
+    return visited;
+}
+
+console.log(dfsIn(tree));
